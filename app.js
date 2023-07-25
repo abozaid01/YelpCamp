f333b022d1ffef875770f3d6bfa883a9c0eae484
Mohamed Abozaid <mohamed.abozaid01@gmail.com>
2023-07-25 18:21:37 +0300

    implement basic CRUD functionality for Campgrounds

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const methodOverrided = require('method-override');
 
 //db connection
 const mongoose = require('mongoose');
@@ -15,6 +16,9 @@ const Campground = require('./models/campgroud');
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverrided('_method'));
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
@@ -23,10 +27,44 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/makecampgroudn', async (req, res) => {
-  const camp = new Campground({ title: 'My Backyard' });
-  await camp.save();
-  res.send(camp);
+//CRUD
+//Create
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+  await Campground.create(req.body.campground);
+  res.redirect('/campgrounds');
+});
+
+//Read All
+app.get('/campgrounds', async (req, res) => {
+  const allCamps = await Campground.find();
+  res.render('campgrounds/index', { allCamps });
+});
+
+//Read one
+app.get('/campgrounds/:_id', async (req, res) => {
+  const camp = await Campground.findById(req.params._id);
+  res.render('campgrounds/show', { camp });
+});
+
+//Update
+app.get('/campgrounds/:_id/edit', async (req, res) => {
+  const camp = await Campground.findById(req.params._id);
+  res.render('campgrounds/edit', { camp });
+});
+
+app.put('/campgrounds/:_id', async (req, res) => {
+  await Campground.findByIdAndUpdate(req.params._id, req.body.campground);
+  res.redirect(`/campgrounds/${req.params._id}`);
+});
+
+//Delete
+app.delete('/campgrounds/:_id', async (req, res) => {
+  await Campground.findByIdAndDelete(req.params._id, req.body.ampground);
+  res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
